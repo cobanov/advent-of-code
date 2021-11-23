bfb0dddfb0f92927caa0fc79baa0d646a520fd0a
Mert Cobanov <mertcobanov@gmail.com>
2021-11-23 04:33:15 +0300

    day 10 added

---
+++ 2021/day10.py
@@ -0,0 +1,39 @@
+from ftplib import error_proto
+
+
+with open('./inputs/day10.txt') as file:
+
+    document = [line.strip() for line in file]
+    # Rules
+    rules = document[:20]
+    rules = [line.split(':')[1] for line in rules]
+    rules = [line.strip().split('or') for line in rules]
+    rules = [val.strip() for sublist in rules for val in sublist]
+
+    # Nearby tickets
+    nearby = document[25:]
+
+# Calculate Space
+
+space = []
+
+for interval in rules:
+    min, max = interval.split('-')
+    space.extend(range(int(min), int(max)))
+space = set(space)
+
+####
+
+
+# # Nearby Tickets
+error_rate = 0
+
+for ticket in nearby:
+    numbers = ticket.split(',')
+    integer_list = list(map(int, numbers))
+    for number in integer_list:
+        if number not in space:
+            error_rate += number
+            print('Invalid', number)
+
+print(error_rate)
