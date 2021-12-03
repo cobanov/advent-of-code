e11de7b9787b5ee063f87b30317974bf1464f92c
Mert Cobanov <mertcobanov@gmail.com>
2021-12-03 15:26:00 +0300

    day 3 part 2

--- 2021/day3.py
+++ 2021/day3.py
@@ -3,10 +3,9 @@ import collections
 with open("./inputs/day3.txt") as file:
     data = [line.strip() for line in file]
 
-
 bits = collections.defaultdict(list)
 
-for i in range(12):
+for i in range(len(data[0])):
     for number in data:
         bits[i].append(number[i])
 
@@ -21,4 +20,49 @@ for key, value in bits.items():
 most_int = int(''.join(most_bits), 2)
 least_int = int(''.join(least_bits), 2)
 
-print(most_int * least_int)
+print('Answer 1: ', most_int * least_int)
+
+
+# Part 2
+oxygen = data
+oxygen_rating = 0
+
+for i in range(len(data[0])):
+    bit_counter = {'0': 0, '1': 0}
+
+    for number in oxygen:
+        bit_counter[number[i]] += 1
+
+    if bit_counter['1'] >= bit_counter['0']:
+        most_common = '1'
+
+    else:
+        most_common = '0'
+
+    oxygen = [n for n in oxygen if n[i] == most_common]
+
+    if len(oxygen) == 1:
+        oxygen_rating = int(oxygen[0], 2)
+
+co2 = data
+co2_rating = 0
+
+for i in range(len(data[0])):
+    bit_counter = {'0': 0, '1': 0}
+
+    for number in co2:
+        bit_counter[number[i]] += 1
+
+    if bit_counter['1'] < bit_counter['0']:
+        least_common = '1'
+
+    else:
+        least_common = '0'
+
+    co2 = [n for n in co2 if n[i] == least_common]
+
+    if len(co2) == 1:
+        co2_rating = int(co2[0], 2)
+
+
+print('Answer 2: ', co2_rating * oxygen_rating)
