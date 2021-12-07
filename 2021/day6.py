cf4f44cc051c08bd97b3e27daa8ef9659fe96922
Mert Cobanov <mertcobanov@gmail.com>
2021-12-07 09:23:23 +0300

    day6

--- 2021/day6.py
+++ 2021/day6.py
@@ -2,14 +2,14 @@ from collections import Counter
 with open('./inputs/test.txt') as file:
     fishes = [int(i) for i in file.read().split(',')]
 
-# for iteration in range(21):
-#     for index, value in enumerate(fishes):
-#         fishes[index] -= 1
-#         if fishes[index] == -1:
-#             fishes.append(9)
-#             fishes[index] = 6
+for iteration in range(21):
+    for index, value in enumerate(fishes):
+        fishes[index] -= 1
+        if fishes[index] == -1:
+            fishes.append(9)
+            fishes[index] = 6
 
-#     print('I:', iteration+1, 'C:', len(fishes), fishes)
+    print('I:', iteration+1, 'C:', len(fishes), fishes)
 
 
 fish_population = Counter(fishes)
