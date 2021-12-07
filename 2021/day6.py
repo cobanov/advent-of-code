98de199a43a83ed8a22a28415ec9030a9f92da06
Mert Cobanov <mertcobanov@gmail.com>
2021-12-07 09:14:33 +0300

    day 6

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,11 +1,18 @@
-with open('./inputs/day6.txt') as file:
+from collections import Counter
+with open('./inputs/test.txt') as file:
     fishes = [int(i) for i in file.read().split(',')]
 
-for _ in range(18):
-    for index, value in enumerate(fishes):
-        fishes[index] -= 1
-        if fishes[index] == -1:
-            fishes.append(9)
-            fishes[index] = 6
+# for iteration in range(21):
+#     for index, value in enumerate(fishes):
+#         fishes[index] -= 1
+#         if fishes[index] == -1:
+#             fishes.append(9)
+#             fishes[index] = 6
 
-print('Iteration:', len(fishes))
+#     print('I:', iteration+1, 'C:', len(fishes), fishes)
+
+
+fish_population = Counter(fishes)
+day = 26
+for i in range(day):
+    print(i)
---
+++ 2021/day7.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+with open('./inputs/day7.txt') as file:
+    numbers = [int(i) for i in file.read().split(',')]
+
+# Part 1
+steps = numbers - np.median(numbers)
+print('Answer 1: ', sum(abs(steps)))
+
+
+# Part 2
+steps = abs(numbers - np.round(np.mean(numbers))-1)
+sum_list = [sum(i) for i in [range(int(i+1)) for i in steps]]
+print('Answer 2: ', sum(sum_list))
