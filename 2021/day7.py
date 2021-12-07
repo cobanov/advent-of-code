6d2e7eb89151df1366dd0088fc94ec4f962184b0
Mert Cobanov <mertcobanov@gmail.com>
2021-12-07 09:14:53 +0300

    day 7 added

--- 2021/day7.py
+++ 2021/day7.py
@@ -7,7 +7,6 @@ with open('./inputs/day7.txt') as file:
 steps = numbers - np.median(numbers)
 print('Answer 1: ', sum(abs(steps)))
 
-
 # Part 2
 steps = abs(numbers - np.round(np.mean(numbers))-1)
 sum_list = [sum(i) for i in [range(int(i+1)) for i in steps]]
